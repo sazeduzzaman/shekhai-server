a1bb3155a74c7005f0b4dad1117f6b66c1138465
Szamansaju <szamansaju@gmail.com>
2025-11-18 17:49:24 +0600

    File Updated

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json({limit: '10mb'}));
 app.use(morgan('dev'));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 connectDB();
 
